45b78445dc19b7bc2ee4fd5c86b574e85422d250
Ole Wunschmenn <owunschmann4046@cosmoconsult.com>
2025-07-23 18:45:41 +0200

    feat: refactor

--- Actions/Initialization/Initialization.ps1
+++ Actions/Initialization/Initialization.ps1
@@ -17,7 +17,7 @@ Write-Host "Using Scripts Archive Url '$scriptsArchiveUrl'"
 Add-Content -encoding UTF8 -Path $env:GITHUB_ENV -Value "ALPACA_SCRIPTS_ARCHIVE_URL=$($scriptsArchiveUrl)"
 Add-Content -encoding UTF8 -Path $env:GITHUB_OUTPUT -Value "scriptsArchiveUrl=$($scriptsArchiveUrl)"
 
-$scriptsArchiveDirectory = "./Scripts"
+$scriptsArchiveDirectory = "./*/Scripts"
 Write-Host "Using Scripts Archive Directory '$scriptsArchiveDirectory'"
 Add-Content -encoding UTF8 -Path $env:GITHUB_ENV -Value "ALPACA_SCRIPTS_ARCHIVE_DIRECTORY=$($scriptsArchiveDirectory)"
 Add-Content -encoding UTF8 -Path $env:GITHUB_OUTPUT -Value "scriptsArchiveDirectory=$($scriptsArchiveDirectory)"
