d83a19faf5f3567f3eeedddfba0921cb7e0a17fe
Ole Wunschmenn <owunschmann4046@cosmoconsult.com>
2025-07-23 21:38:55 +0200

    feat: refactor grouping

--- Scripts/Modules/Publish-AlpacaBcApp.psm1
+++ Scripts/Modules/Publish-AlpacaBcApp.psm1
@@ -75,7 +75,7 @@ function Publish-AlpacaBcApp {
             $success = $true
         }
         catch {
-            $errorMessage = Get-ExtendedErrorMessage -errorRecord $_
+            $errorMessage = Get-AlpacaExtendedErrorMessage -errorRecord $_
             $errorMessage = "Error Publishing App '$appName'`n$errorMessage"
 
             $tries = $tries + 1
