a5a6fbe0fe8279b558de22e3be6776fc15ee05df
Ole Wunschmenn <owunschmann4046@cosmoconsult.com>
2025-07-23 19:48:57 +0200

    feat: refactor

--- Scripts/Overrides/RunAlPipeline/PipelineInitialize.ps1
+++ Scripts/Overrides/RunAlPipeline/PipelineInitialize.ps1
@@ -7,24 +7,24 @@ param(
     [object] $CreateContainersJob
 )
 
-Import-Module (Join-Path $ScriptsPath "Modules\Alpaca.psd1") -Scope Global -DisableNameChecking
+Import-Module (Join-Path $ScriptsPath "Modules/Alpaca.psd1") -Scope Global -DisableNameChecking
 
 # Collect informations
 
 Write-Host "Getting AL-Go project from environmental variable"
 $project = $env:_project
 
-Write-Host "Getting Alpaca backend url from initialization job outputs"
+Write-Host "Getting Alpaca backend url from outputs of initialization job"
 $backendUrl = $InitializationJob.outputs.backendUrl
 
-Write-Host "Geting Alpaca container information from create containers job outputs"
+Write-Host "Getting Alpaca container information from outputs of create containers job"
 $containers = @("$($CreateContainersJob.outputs.containersJson)" | ConvertFrom-Json)
 $container = $containers | Where-Object { $_.Project -eq $project }
 if (! $container) {
     throw "No Alpaca container information for project '$project' found"
 }
 
-Write-Host "Getting Alpaca container authentication context from container information"
+Write-Host "Getting container authentication context from Alpaca container information"
 $containerAuthContext = @{
     username = $container.User
     Password = ConvertTo-SecureString -String $container.Password -AsPlainText
@@ -62,31 +62,26 @@ Get-AlpacaDependencyApps -packagesFolder $packagesFolder -token $env:_token
 
 # Load overrides
 
-$alpacaOverrides = $runAlPipelineOverrides | Where-Object { $_ -ne "PipelineInitialize"}
-if ($alpacaOverrides) {
+$overridesPath = Join-Path $ScriptsPath "Overrides/RunAlPipeline"
 
-    Write-Host "Loading Alpaca overrides"
+Write-Host "Loading Alpaca overrides from $(Resolve-Path $overridesPath -Relative)"
+Get-ChildItem -Path $overridesPath -File -Filter "*.ps1" -Exclude "PipelineInitialize.*" | ForEach-Object {
+    $scriptPath = $_.FullName
+    $scriptName = $_.BaseName
 
-    $alpacaOverrides | ForEach-Object {
-        $scriptName = $_
-        $scriptPath = Join-Path $ScriptsPath "Overrides\RunAlPipeline\$scriptName.ps1"
+    Write-Host "Loading Alpaca override for '$scriptName' from '$(Resolve-Path $scriptPath -Relative)'"
+    $scriptBlock = Get-Command $scriptPath | Select-Object -ExpandProperty ScriptBlock
 
-        if (Test-Path -Path $scriptPath -Type Leaf) {
-            Write-Host "Getting existing override for '$scriptName'"
-            $existingScriptBlock = Get-Variable -Name $scriptName -ValueOnly -Scope 1 -ErrorAction Ignore
-            if ($existingScriptBlock) {
-                Write-Host -ForegroundColor Yellow "Existing '$scriptName' override"; 
-                Write-Host $existingScriptBlock.ToString()
+    Write-Host "Getting existing override for '$scriptName'"
+    $existingScriptBlock = Get-Variable -Name $scriptName -ValueOnly -Scope 1 -ErrorAction Ignore
+    if ($existingScriptBlock) {
+        Write-Host -ForegroundColor Yellow "Existing '$scriptName' override"; 
+        Write-Host $existingScriptBlock.ToString()
 
-                Write-Host "Setting parent 'AlGo$ScriptName' to existing override"
-                Set-Variable -Name "AlGo$scriptName" -Value $existingScriptBlock -Scope 1
-            }
-
-            Write-Host "Loading Alpaca override for '$scriptName' with '$scriptPath'"
-            $scriptBlock = Get-Command $scriptPath | Select-Object -ExpandProperty ScriptBlock
-
-            Write-Host "Setting parent '$scriptName' to Alpaca override"
-            Set-Variable -Name $scriptName -Value $scriptBlock -Scope 1
-        }
+        Write-Host "Setting parent 'AlGo$ScriptName' to existing override"
+        Set-Variable -Name "AlGo$scriptName" -Value $existingScriptBlock -Scope 1
     }
+
+    Write-Host "Setting parent '$scriptName' to Alpaca override"
+    Set-Variable -Name $scriptName -Value $scriptBlock -Scope 1
 }
