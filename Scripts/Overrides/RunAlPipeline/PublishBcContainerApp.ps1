493d30675ab08fcfc588dbd43775e0feee4c9b8e
Ole Wunschmenn <owunschmann4046@cosmoconsult.com>
2025-08-29 10:26:11 +0200

    Publishing w/ AppInfos

--- Scripts/Overrides/RunAlPipeline/PublishBcContainerApp.ps1
+++ Scripts/Overrides/RunAlPipeline/PublishBcContainerApp.ps1
@@ -4,51 +4,58 @@ Param(
 
 Write-AlpacaOutput "Using COSMO Alpaca override"
 
-$publishedAppFiles = Get-Variable -Name alpacaPublishedAppFiles -ValueOnly -Scope Script -ErrorAction Ignore
-if (! $publishedAppFiles) {
-    $publishedAppFiles = @()
+$publishedAppInfos = Get-Variable -Name alpacaPublishedAppInfos -ValueOnly -Scope Script -ErrorAction Ignore
+if (! $publishedAppInfos) {
+    $publishedAppInfos = @()
 }
+
+$compilerFolder = (GetCompilerFolder)
 
 $outputAppFiles = $apps + $testApps + $bcptTestApps | Resolve-Path | Select-Object -ExpandProperty Path
 $previousAppFiles = $previousApps | Resolve-Path | Select-Object -ExpandProperty Path
 $installAppFiles = $installApps + ($installTestApps -replace '^\(|\)$') | Resolve-Path | Select-Object -ExpandProperty Path
 
 $dependenciesFolder = Join-Path "$env:GITHUB_WORKSPACE" ".dependencies"
-$dependencyAppFileHashs = 
+$dependencyAppFiles = 
     Get-ChildItem -Path $dependenciesFolder -File -Recurse |
-    Where-Object { $installAppFiles -contains $_.FullName } |
-    ForEach-Object { (Get-FileHash -Path $_).Hash }
+    Select-Object -ExpandProperty FullName |
+    Where-Object { $installAppFiles -contains $_ }
+$dependencyAppInfos = @(GetAppInfo -AppFiles $dependencyAppFiles -compilerFolder $compilerFolder -cacheAppinfoPath (Join-Path $dependenciesFolder 'cache_AppInfo.json'))
 
 Write-AlpacaGroupStart "Apps:"
 
-$appFiles = @();
+$appInfos = @();
 foreach ($appFile in $parameters.appFile) {
     $appFile = (Resolve-Path -Path $appFile).Path
+    $appInfo = GetAppInfo -AppFiles $appFile -compilerFolder $compilerFolder -cacheAppinfoPath (Join-Path (Split-Path $appFile -Parent) 'cache_AppInfo.json')
+    $appLabel = '{0}, {1}, {2}, {3}' -f $appInfo.Publisher, $appInfo.Name, $appInfo.Id, $appInfo.Version
 
-    if ($publishedAppFiles -contains $appFile) {
+    # Skip unhandled apps
+    $appComment = "skip"
+
+    if ($publishedAppInfos | Where-Object { $_.id -eq $appInfo.id -and $_.version -eq $appInfo.version }) {
         # Skip already published apps
-        Write-AlpacaOutput "- skip '$appFile' (already published)"
+        $appComment = "skip already published"
     } elseif ($outputAppFiles -contains $appFile) {
         # Publish output apps
-        Write-AlpacaOutput "- publish '$appFile' (build output)"
-        $appFiles += $appFile
+        $appComment = "publish build output"
+        $appInfos += $appInfo
     } elseif ($previousAppFiles -contains $appFile) {
         # Publish previous apps
-        Write-AlpacaOutput "- publish '$appFile' (previous release)"
-        $appFiles += $appFile
-    } elseif ($dependencyAppFileHashs -contains (Get-FileHash -Path $appFile).Hash) {
+        $appComment = "publish previous release"
+        $appInfos += $appInfo
+    } elseif ($dependencyAppInfos | Where-Object { $_.id -eq $appInfo.id -and $_.version -eq $appInfo.version }) {
         # Publish dependency apps
-        Write-AlpacaOutput "- publish '$appFile' (project dependency)"
-        $appFiles += $appFile
-    } else {
-        # Skip remaining apps
-        Write-AlpacaOutput "- skip '$appFile'"
+        $appComment = "publish dependency build output"
+        $appInfos += $appInfo
     }
+
+    Write-AlpacaOutput "- $appComment '$appFile' ($appLabel)"
 }
 
 Write-AlpacaGroupEnd
 
-if ($appFiles) {
+if ($appInfos) {
     Write-AlpacaGroupStart "Wait for image to be ready"
     if ($env:ALPACA_CONTAINER_IMAGE_READY) {
         Write-AlpacaOutput "ALPACA_CONTAINER_IMAGE_READY is already set to '$env:ALPACA_CONTAINER_IMAGE_READY'. Skipping wait."
@@ -71,17 +78,17 @@ if ($appFiles) {
 
     $password = ConvertFrom-SecureString -SecureString $parameters.bcAuthContext.Password -AsPlainText
 
-    foreach($appFile in $appFiles) {
+    foreach($appInfo in $appInfos) {
         Publish-AlpacaBcApp -ContainerUrl $parameters.Environment `
                             -ContainerUser $parameters.bcAuthContext.username `
                             -ContainerPassword $password `
-                            -Path $appFile
+                            -Path $appInfo.Path
     }
 
-    $publishedAppFiles += $appFiles
+    $publishedAppInfos += $appInfos
 }
 
-Set-Variable -Name alpacaPublishedAppFiles -Value $publishedAppFiles -Scope Script
+Set-Variable -Name alpacaPublishedAppInfos -Value $publishedAppInfos -Scope Script
 
 if ($AlGoPublishBcContainerApp) {
     Write-AlpacaOutput "Invoking AL-Go override"
