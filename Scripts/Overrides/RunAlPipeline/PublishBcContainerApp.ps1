228ca6ddab3800c22216165b48ce211b41b70a5f
Ole Wunschmann <44057549+OleWunschmann@users.noreply.github.com>
2025-09-04 16:24:46 +0200

    feat: support for multi-project dependencies (#5)
    [AB#4452](https://dev.azure.com/cc-ppi/83f75d99-795d-45dc-8543-9fe1918ff7f9/_workitems/edit/4452)
    
    ---------
    
    Co-authored-by: Ole Wunschmenn <owunschmann4046@cosmoconsult.com>

--- Scripts/Overrides/RunAlPipeline/PublishBcContainerApp.ps1
+++ Scripts/Overrides/RunAlPipeline/PublishBcContainerApp.ps1
@@ -4,36 +4,70 @@ Param(
 
 Write-AlpacaOutput "Using COSMO Alpaca override"
 
-$skip = $false
-if ($parameters.appFile.GetType().BaseType.Name -eq 'Array') {
-    # Check if current run is installing dependenciy apps
-    # Dependency apps are already installed and should be skipped
-    $equal = $true
-    for ($i = 0; $i -lt $appsBeforeApps.Count; $i++) {
-        if ($appsBeforeApps[$i] -ne $parameters.appFile[$i]) {
-            $equal = $false
-            break
-        }
-    }
-
-    if (-not $equal) {
-        #check second dependency array
-        $equal = $true
-        for ($i = 0; $i -lt $appsBeforeTestApps.Count; $i++) {
-            if ($appsBeforeTestApps[$i] -ne $parameters.appFile[$i]) {
-                $equal = $false
-                break
-            }
-        }
-    }
-
-    if ($equal) {
-        Write-AlpacaOutput "Skip apps before apps/testapps because they are already handled by Alpaca"
-        $skip = $true
-    }
+$publishedAppInfos = Get-Variable -Name alpacaPublishedAppInfos -ValueOnly -Scope Script -ErrorAction Ignore
+if (! $publishedAppInfos) {
+    $publishedAppInfos = @()
 }
 
-if (! $skip) {
+# Collect app files
+$outputAppFiles = $apps + $testApps + $bcptTestApps | Resolve-Path | Select-Object -ExpandProperty Path
+$previousAppFiles = $previousApps | Resolve-Path | Select-Object -ExpandProperty Path
+$installAppFiles = $installApps + ($installTestApps -replace '^\(|\)$') | Resolve-Path | Select-Object -ExpandProperty Path
+
+# Collect parameter app infos
+$appInfos = @()
+if ($parameters.appFile) {
+    $compilerFolder = (GetCompilerFolder)
+    $appInfos += GetAppInfo -AppFiles $parameters.appFile -compilerFolder $compilerFolder -cacheAppinfoPath (Join-Path $packagesFolder 'cache_AppInfo.json')
+}
+
+# Collect dependency app infos
+$dependenciesFolder = Join-Path "$env:GITHUB_WORKSPACE" ".dependencies"
+$dependencyAppFiles = @()
+$dependencyAppInfos = @()
+if (Test-Path $dependenciesFolder) {
+    $dependencyAppFiles += Get-ChildItem -Path $dependenciesFolder -File -Recurse |
+        Select-Object -ExpandProperty FullName |
+        Where-Object { $installAppFiles -contains $_ }
+}
+if ($dependencyAppFiles) {
+    $compilerFolder = (GetCompilerFolder)
+    $dependencyAppInfos += GetAppInfo -AppFiles $dependencyAppFiles -compilerFolder $compilerFolder -cacheAppinfoPath (Join-Path $dependenciesFolder 'cache_AppInfo.json')
+}
+
+Write-AlpacaGroupStart "Apps:"
+
+$appInfos = $appInfos | ForEach-Object {
+    $appInfo = $_
+    $appFile = (Resolve-Path -Path $appInfo.Path).Path
+    $appLabel = '{0}, {1}, {2}, {3}' -f $appInfo.Id, $appInfo.Name, $appInfo.Publisher, $appInfo.Version
+
+    # Skip unhandled apps
+    $appComment = "skip"
+
+    if ($publishedAppInfos | Where-Object { $_.Id -eq $appInfo.Id -and $_.Version -eq $appInfo.Version }) {
+        # Skip already published apps
+        $appComment = "skip already published"
+    } elseif ($outputAppFiles -contains $appFile) {
+        # Publish output apps
+        $appComment = "publish build output"
+        $appInfo
+    } elseif ($previousAppFiles -contains $appFile) {
+        # Publish previous apps
+        $appComment = "publish previous release"
+        $appInfo
+    } elseif ($dependencyAppInfos | Where-Object { $_.Id -eq $appInfo.Id -and $_.Version -eq $appInfo.Version }) {
+        # Publish dependency apps
+        $appComment = "publish dependency build output"
+        $appInfo
+    }
+
+    Write-AlpacaOutput "- $appComment '$appFile' ($appLabel)"
+}
+
+Write-AlpacaGroupEnd
+
+if ($appInfos) {
     Write-AlpacaGroupStart "Wait for image to be ready"
     if ($env:ALPACA_CONTAINER_IMAGE_READY) {
         Write-AlpacaOutput "ALPACA_CONTAINER_IMAGE_READY is already set to '$env:ALPACA_CONTAINER_IMAGE_READY'. Skipping wait."
@@ -44,7 +78,7 @@ if (! $skip) {
     }
     Write-AlpacaGroupEnd
 
-    Write-AlpacaGroupStart "Wait for container start"
+    Write-AlpacaGroupStart "Wait for container to be ready"
     if ($env:ALPACA_CONTAINER_READY) {
         Write-AlpacaOutput "ALPACA_CONTAINER_READY is already set to '$env:ALPACA_CONTAINER_READY'. Skipping wait."
     } else {
@@ -54,13 +88,17 @@ if (! $skip) {
     }
     Write-AlpacaGroupEnd
 
-    Write-AlpacaOutput "Get password from SecureString"
     $password = ConvertFrom-SecureString -SecureString $parameters.bcAuthContext.Password -AsPlainText
 
-    Publish-AlpacaBcApp -ContainerUrl $parameters.Environment `
-                        -ContainerUser $parameters.bcAuthContext.username `
-                        -ContainerPassword $password `
-                        -Path $parameters.appFile
+    foreach($appInfo in $appInfos) {
+        Publish-AlpacaBcApp -ContainerUrl $parameters.Environment `
+                            -ContainerUser $parameters.bcAuthContext.username `
+                            -ContainerPassword $password `
+                            -Path $appInfo.Path
+    }
+
+    $publishedAppInfos += $appInfos
+    Set-Variable -Name alpacaPublishedAppInfos -Value $publishedAppInfos -Scope Script
 }
 
 if ($AlGoPublishBcContainerApp) {
