8928e1d32271aaf92b6249f9cc5332e12d7a4518
Christoph Blank <christoph.blank@cosmoconsult.com>
2025-10-14 11:31:48 +0200

    feat: add Get-AlpacaAppInfo function and integrate it into PublishBcContainerApp script AB#4489

---
+++ Scripts/Modules/Get-AlpacaAppInfo.psm1
@@ -0,0 +1,27 @@
+function Get-AlpacaAppInfo {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $ContainerName,
+        [Parameter(Mandatory = $true)]
+        [string] $Token
+    )
+    process {
+        $owner = $env:GITHUB_REPOSITORY_OWNER
+        $repository = $env:GITHUB_REPOSITORY
+        $repository = $repository.replace($owner, "")
+        $repository = $repository.replace("/", "")
+
+        $headers = Get-AlpacaAuthenticationHeaders -Token $Token -Owner $owner -Repository $repository
+        $headers.add("accept", "application/text")
+
+        $QueryParams = @{
+            "api-version" = "1.0"
+        }
+        $apiUrl = Get-AlpacaEndpointUrlWithParam -Api 'alpaca' -Controller "Container" -Endpoint "Exec" -Ressource $ContainerName -RouteSuffix "appInfo" -QueryParams $QueryParams
+
+        $result = Invoke-RestMethod $apiUrl -Method 'Get' -Headers $headers -AllowInsecureRedirect
+        return $result
+    }
+}
+
+Export-ModuleMember -Function Get-AlpacaAppInfo
--- Scripts/Overrides/RunAlPipeline/PublishBcContainerApp.ps1
+++ Scripts/Overrides/RunAlPipeline/PublishBcContainerApp.ps1
@@ -6,7 +6,13 @@ Write-AlpacaOutput "Using COSMO Alpaca override"
 
 $publishedAppInfos = Get-Variable -Name alpacaPublishedAppInfos -ValueOnly -Scope Script -ErrorAction Ignore
 if (! $publishedAppInfos) {
+    try {
+        $publishedAppInfos = Get-AlpacaAppInfo -Token $env:_token -ContainerName $env:ALPACA_CONTAINER_ID
+    }
+    catch {
+        Write-AlpacaOutput "Error occurred while getting published app infos: $_"
     $publishedAppInfos = @()
+}
 }
 
 # Collect app files
