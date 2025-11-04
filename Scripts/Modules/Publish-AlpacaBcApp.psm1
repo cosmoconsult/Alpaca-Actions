7f1798ab2a279f72a69a9411b5ff8428b481151d
Ole Wunschmenn <owunschmann4046@cosmoconsult.com>
2025-11-05 00:32:52 +0100

    fix(build): publish apps in correct container tenant

--- Scripts/Modules/Publish-AlpacaBcApp.psm1
+++ Scripts/Modules/Publish-AlpacaBcApp.psm1
@@ -52,7 +52,7 @@ function Publish-AlpacaBcApp {
                 $Tenant = $queryDict["tenant"]
             }
         }
-        $devServerUrl = $url.TrimEnd('/') + "/dev/dev/apps?SchemaUpdateMode=$schemaUpdateMode&tenant=$Tenant"
+        $devServerUrl = $url.TrimEnd('/') + "dev/dev/apps?SchemaUpdateMode=$schemaUpdateMode&tenant=$Tenant"
     
         $multipartContent = [System.Net.Http.MultipartFormDataContent]::new()
         $FileStream = [System.IO.FileStream]::new($Path, [System.IO.FileMode]::Open)
