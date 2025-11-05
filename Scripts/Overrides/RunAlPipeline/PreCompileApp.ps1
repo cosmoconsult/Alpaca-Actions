2238fecd973e132fe59839f5575c6f66f8ca834d
Christoph Blank <christoph.blank@cosmoconsult.com>
2025-11-05 14:57:03 +0100

    refactor(script): enhance PreCompileApp with error handling and environment logging

--- Scripts/Overrides/RunAlPipeline/PreCompileApp.ps1
+++ Scripts/Overrides/RunAlPipeline/PreCompileApp.ps1
@@ -2,5 +2,23 @@ param(
     [string] $appType,
     [ref] $compilationParams
 )
-Write-Host "Hello Alpaca Overwrite"
-Invoke-AlpacaPrecompileApp -appType $appType -compilationParams ([ref] $compilationParams)
+Write-Host "Hello from Alpaca Overwrite"
+
+try {
+    Write-Host "import Module"
+    Import-Module (Join-Path -Path $PSScriptRoot -ChildPath "..\..\Scripts\Modules\Alpaca.psd1" -Resolve)
+    Write-Host "Invoke Invoke-AlpacaPrecompileApp"
+    Invoke-AlpacaPrecompileApp -appType $appType -compilationParams ([ref] $compilationParams)
+}
+finally {
+    Write-Host "Env:"
+    Get-ChildItem Env: | ForEach-Object { Write-Host "  $($_.Name): $($_.Value)" }
+    Write-Host "Alpaca Cmdlets:"
+    Get-Command *Alpaca* | ForEach-Object { Write-Host "  $($_.Name)" }
+}
+
+   
+
+
+
+
