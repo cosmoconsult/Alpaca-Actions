79739dddc6ace6decc37974a2bfe61e832f36ba7
Christoph Blank <christoph.blank@cosmoconsult.com>
2025-11-05 15:53:56 +0100

    refactor(script): streamline PreCompileApp by removing redundant logging and error handling

--- Scripts/Modules/Invoke-AlpacaPrecompileApp.psm1
+++ Scripts/Modules/Invoke-AlpacaPrecompileApp.psm1
@@ -4,6 +4,41 @@ function Invoke-AlpacaPrecompileApp {
         [ref] $compilationParams
     )
     Write-Host "Hello from Invoke-AlpacaPrecompileApp.psm1"
+
+    #region DebugInfo
+    Write-AlpacaGroupStart "DebugInfo"
+
+    Write-Host "App Type: $appType"
+
+    Write-Host "Compilation Params:"
+    $compilationParams.Value | ForEach-Object { Write-Host "  $_" }
+
+    Write-Host "Env Variables:"
+    Get-ChildItem Env: | ForEach-Object { "  $($_.Name): $($_.Value)" }
+
+    Write-AlpacaGroupEnd
+    #endregion DebugInfo
+
+    # TODO: check if xliff things configured
+    Write-AlpacaGroupStart "Precompile and Translate"
+
+    #region PreCompile
+
+    # Remove Cops parameters
+    $compilationParamsCopy = $compilationParams.Clone()
+    $compilationParamsCopy.Keys | Where-Object { $_ -in $CopParameters.Keys } | ForEach-Object { $compilationParams.Remove($_) }
+    # TODO: Check other compilationParamsCopy
+    if ($useCompilerFolder) {
+        $appFile = Invoke-Command -ScriptBlock $CompileAppWithBcCompilerFolder -ArgumentList $compilationParamsCopy
+    }
+    else {
+        $appFile = Invoke-Command -ScriptBlock $CompileAppInBcContainer -ArgumentList $compilationParamsCopy
+    }
+    #endregion PreCompile
+
+    # TODO: merge xliff
+    Write-AlpacaGroupEnd
+
 }
 
 Export-ModuleMember -Function Invoke-AlpacaPrecompileApp
--- Scripts/Overrides/RunAlPipeline/PreCompileApp.ps1
+++ Scripts/Overrides/RunAlPipeline/PreCompileApp.ps1
@@ -2,21 +2,6 @@ param(
     [string] $appType,
     [ref] $compilationParams
 )
-Write-Host "Hello from Alpaca Overwrite"
+Write-AlpacaOutput "Using COSMO Alpaca override"
 
-try {
-    Write-Host "Invoke Invoke-AlpacaPrecompileApp"
-    Invoke-AlpacaPrecompileApp -appType $appType -compilationParams ([ref] $compilationParams)
-}
-finally {
-    Write-Host "Env:"
-    Get-ChildItem Env: | ForEach-Object { Write-Host "  $($_.Name): $($_.Value)" }
-    Write-Host "Alpaca Cmdlets:"
-    Get-Command *Alpaca* | ForEach-Object { Write-Host "  $($_.Name)" }
-}
-
-   
-
-
-
-
+Invoke-AlpacaPrecompileApp -appType $appType -compilationParams ([ref] $compilationParams)
