9f455b479f6a106e000e2c61f9c90f479b4a4cc1
Christoph Blank <christoph.blank@cosmoconsult.com>
2025-11-07 11:26:01 +0100

    fix(script): add Translation-Helper module and remove translation functions from PreCompileApp

--- Scripts/Overrides/RunAlPipeline/PreCompileApp.ps1
+++ Scripts/Overrides/RunAlPipeline/PreCompileApp.ps1
@@ -4,89 +4,6 @@ param(
 )
 Write-AlpacaOutput "Using COSMO Alpaca override"
 
-#region Functions
-function New-TranslationFile() {
-    # Create translation files (e.g. .de-DE.xlf) based on existing .g.xlf
-    param(
-        [Parameter(Mandatory = $true)]
-        [string]$Folder,
-        [ValidateScript({ <# en-US,de-DE,de-AT,... #> $_ -cmatch '^[a-z]{2}-[A-Z]{2}$' })]
-        [string[]]$Languages = @()
-    )
-    Write-Host "##[section]Create Translations ($($Languages -join ','))"
-
-    Install-Module -Name XliffSync -Scope CurrentUser -Force
-    Write-AlpacaOutput "Successfully installed XliffSync module"
-
-    Write-AlpacaOutput "Found $($Languages.Count) target languages"
-
-    $GlobalXlfFiles = @() # Initialize variable to enforce an array due to strict mode
-    $GlobalXlfFiles += Get-ChildItem -Path $Folder -Include '*.g.xlf' -Recurse
-    if (-not $GlobalXlfFiles) {
-        Write-AlpacaError "No .g.xlf files found in $Folder!"
-        Write-AlpacaOutput ("Files in directory: {0}" -f ((Get-ChildItem -Path $Folder -Recurse | Select-Object -ExpandProperty FullName -ErrorAction SilentlyContinue | ForEach-Object { $_.Replace($Folder, '').TrimStart('\') } )) -join ', ')
-        throw
-    }
-    Write-AlpacaOutput "Found $($GlobalXlfFiles.Count) files in $Folder"
-
-    foreach ($GlobalXlfFile in $GlobalXlfFiles) {
-        $FormatTranslationUnit = { param($TranslationUnit) $TranslationUnit.note | Where-Object from -EQ 'Xliff Generator' | Select-Object -ExpandProperty '#text' }
-
-        foreach ($Language in $Languages) {
-            Sync-XliffTranslations `
-                -sourcePath $GlobalXlfFile.FullName `
-                -targetLanguage $Language `
-                -parseFromDeveloperNote `
-                -parseFromDeveloperNoteOverwrite `
-                -parseFromDeveloperNoteSeparator "||" `
-                -detectSourceTextChanges:$false `
-                -AzureDevOps 'warning' `
-                -printProblems `
-                -FormatTranslationUnit $FormatTranslationUnit
-        }
-    }
-}
-function Test-TranslationFile() {
-    # Test translation files
-    param(
-        [Parameter(Mandatory = $true)]
-        [string[]]$Folder,
-
-        [ValidateSet("All", "ConsecutiveSpacesConsistent", "ConsecutiveSpacesExist", "OptionMemberCount", "OptionLeadingSpaces", "Placeholders", "PlaceholdersDevNote")]
-        [string[]]$Rules = @()
-    )
-    Write-AlpacaOutput "Testing Translations (Rules: $($Rules -join ','))"
-
-    Install-Module -Name XliffSync -Scope CurrentUser -Force
-    Write-AlpacaOutput "Successfully installed XliffSync module"
-
-    $TranslatedXlfFiles = @() # Initialize variable to enforce an array due to strict mode
-    $TranslatedXlfFiles += Get-ChildItem -Path $Folder -Include '*.??-??.xlf' -Exclude '*.g.xlf' -Recurse
-    Write-AlpacaOutput "Found $($TranslatedXlfFiles.Count) files in $Folder"
-
-    $Issues = @()
-    foreach ($TranslatedXlfFile in $TranslatedXlfFiles) {
-        $FormatTranslationUnit = { param($TranslationUnit) $TranslationUnit.note | Where-Object from -EQ 'Xliff Generator' | Select-Object -ExpandProperty '#text' }
-
-        $Issues += Test-XliffTranslations `
-            -targetPath $TranslatedXlfFile.FullName `
-            -checkForMissing `
-            -checkForProblems:$( $Rules.Count -gt 0 ) `
-            -translationRules @( $Rules | Where-Object { $_ -ne 'All' } ) `
-            -translationRulesEnableAll:$( $Rules -contains 'All' ) `
-            -AzureDevOps 'warning' `
-            -printProblems `
-            -FormatTranslationUnit $FormatTranslationUnit
-    }
-
-    $IssueCount = $Issues.Count
-    if ($IssueCount -gt 0) {
-        Write-AlpacaError "${issueCount} issues detected in translation files!"
-        throw
-    }
-}
-#endregion Functions
-
 #region DebugInfo
 Write-AlpacaGroupStart "DebugInfo" #Level 1
 if ($env:RUNNER_DEBUG -eq '1' -or $env:GITHUB_RUN_ATTEMPT -gt 1) {
@@ -98,10 +15,10 @@ if ($env:RUNNER_DEBUG -eq '1' -or $env:GITHUB_RUN_ATTEMPT -gt 1) {
 }
 $Settings = $env:Settings | ConvertFrom-Json
 Write-AlpacaOutput "Settings:"
-Write-AlpacaOutput ("Settings.alpaca.createTranslations = {0}" -f $(try { $Settings.alpaca.createTranslations }catch { "" }))
-Write-AlpacaOutput ("Settings.alpaca.translationLanguages = {0}" -f $(try { $Settings.alpaca.translationLanguages -join ', ' }catch { "" }))
-Write-AlpacaOutput ("Settings.alpaca.TestTranslations = {0}" -f $(try { $Settings.alpaca.TestTranslations }catch { "" }))
-Write-AlpacaOutput ("Settings.alpaca.testTranslationRules = {0}" -f $(try { $Settings.alpaca.testTranslationRules -join ', ' }catch { "" }))
+Write-AlpacaOutput ("Settings.alpaca.createTranslations = {0}" -f $(try { $Settings.alpaca.createTranslations }catch { '' }))
+Write-AlpacaOutput ("Settings.alpaca.translationLanguages = {0}" -f $(try { $Settings.alpaca.translationLanguages -join ', ' }catch { '' }))
+Write-AlpacaOutput ("Settings.alpaca.TestTranslations = {0}" -f $(try { $Settings.alpaca.TestTranslations }catch { '' }))
+Write-AlpacaOutput ("Settings.alpaca.testTranslationRules = {0}" -f $(try { $Settings.alpaca.testTranslationRules -join ', ' }catch { '' }))
 
 Write-AlpacaGroupEnd #Level 1
 #endregion DebugInfo
