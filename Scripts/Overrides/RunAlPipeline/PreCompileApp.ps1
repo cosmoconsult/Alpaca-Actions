463ba30c89fcc58c0d6669bb076e2a6cd204f415
Christoph Blank <christoph.blank@cosmoconsult.com>
2025-11-07 09:46:02 +0100

    fix(script): unify function naming and improve variable consistency in PreCompileApp

--- Scripts/Overrides/RunAlPipeline/PreCompileApp.ps1
+++ Scripts/Overrides/RunAlPipeline/PreCompileApp.ps1
@@ -1,16 +1,15 @@
 param(
-    [string] $appType,
-    [ref] $compilationParams
+    [string] $AppType,
+    [ref] $CompilationParams
 )
 Write-AlpacaOutput "Using COSMO Alpaca override"
 
 #region Functions
-function New-TranslationFiles() {
+function New-TranslationFile() {
     # Create translation files (e.g. .de-DE.xlf) based on existing .g.xlf
     param(
         [Parameter(Mandatory = $true)]
         [string]$Folder,
-        
         [ValidateScript({ <# en-US,de-DE,de-AT,... #> $_ -cmatch '^[a-z]{2}-[A-Z]{2}$' })]
         [string[]]$Languages = @()
     )
@@ -21,22 +20,22 @@ function New-TranslationFiles() {
 
     Write-AlpacaOutput "Found $($Languages.Count) target languages"
 
-    $globalXlfFiles = @() # Initialize variable to enforce an array due to strict mode
-    $globalXlfFiles += Get-ChildItem -path $Folder -Include '*.g.xlf' -Recurse
-    if (-not $globalXlfFiles) {
+    $GlobalXlfFiles = @() # Initialize variable to enforce an array due to strict mode
+    $GlobalXlfFiles += Get-ChildItem -Path $Folder -Include '*.g.xlf' -Recurse
+    if (-not $GlobalXlfFiles) {
         Write-AlpacaError "No .g.xlf files found in $Folder!"
-        Write-AlpacaOutput ("Files in directory: {0}" -f ((Get-ChildItem -path $Folder -Recurse | Select-Object -ExpandProperty FullName -ErrorAction SilentlyContinue | ForEach-Object { $_.Replace($Folder, '').TrimStart('\') } )) -join ', ')
+        Write-AlpacaOutput ("Files in directory: {0}" -f ((Get-ChildItem -Path $Folder -Recurse | Select-Object -ExpandProperty FullName -ErrorAction SilentlyContinue | ForEach-Object { $_.Replace($Folder, '').TrimStart('\') } )) -join ', ')
         throw
     }
-    Write-AlpacaOutput "Found $($globalXlfFiles.Count) files in $Folder"
+    Write-AlpacaOutput "Found $($GlobalXlfFiles.Count) files in $Folder"
 
-    foreach ($globalXlfFile in $globalXlfFiles) {
+    foreach ($GlobalXlfFile in $GlobalXlfFiles) {
         $FormatTranslationUnit = { param($TranslationUnit) $TranslationUnit.note | Where-Object from -EQ 'Xliff Generator' | Select-Object -ExpandProperty '#text' }
 
-        foreach ($language in $Languages) {
+        foreach ($Language in $Languages) {
             Sync-XliffTranslations `
-                -sourcePath $globalXlfFile.FullName `
-                -targetLanguage $language `
+                -sourcePath $GlobalXlfFile.FullName `
+                -targetLanguage $Language `
                 -parseFromDeveloperNote `
                 -parseFromDeveloperNoteOverwrite `
                 -parseFromDeveloperNoteSeparator "||" `
@@ -47,7 +46,7 @@ function New-TranslationFiles() {
         }
     }
 }
-function Test-TranslationFiles() {
+function Test-TranslationFile() {
     # Test translation files
     param(
         [Parameter(Mandatory = $true)]
@@ -61,16 +60,16 @@ function Test-TranslationFiles() {
     Install-Module -Name XliffSync -Scope CurrentUser -Force
     Write-AlpacaOutput "Successfully installed XliffSync module"
 
-    $translatedXlfFiles = @() # Initialize variable to enforce an array due to strict mode
-    $translatedXlfFiles += Get-ChildItem -path $Folder -Include '*.??-??.xlf' -Exclude '*.g.xlf' -Recurse
-    Write-AlpacaOutput "Found $($translatedXlfFiles.Count) files in $Folder"
+    $TranslatedXlfFiles = @() # Initialize variable to enforce an array due to strict mode
+    $TranslatedXlfFiles += Get-ChildItem -Path $Folder -Include '*.??-??.xlf' -Exclude '*.g.xlf' -Recurse
+    Write-AlpacaOutput "Found $($TranslatedXlfFiles.Count) files in $Folder"
 
-    $issues = @()
-    foreach ($translatedXlfFile in $translatedXlfFiles) {
+    $Issues = @()
+    foreach ($TranslatedXlfFile in $TranslatedXlfFiles) {
         $FormatTranslationUnit = { param($TranslationUnit) $TranslationUnit.note | Where-Object from -EQ 'Xliff Generator' | Select-Object -ExpandProperty '#text' }
 
-        $issues += Test-XliffTranslations `
-            -targetPath $translatedXlfFile.FullName `
+        $Issues += Test-XliffTranslations `
+            -targetPath $TranslatedXlfFile.FullName `
             -checkForMissing `
             -checkForProblems:$( $Rules.Count -gt 0 ) `
             -translationRules @( $Rules | Where-Object { $_ -ne 'All' } ) `
@@ -80,8 +79,8 @@ function Test-TranslationFiles() {
             -FormatTranslationUnit $FormatTranslationUnit
     }
 
-    $issueCount = $issues.Count
-    if ($issueCount -gt 0) {
+    $IssueCount = $Issues.Count
+    if ($IssueCount -gt 0) {
         Write-AlpacaError "${issueCount} issues detected in translation files!"
         throw
     }
@@ -91,18 +90,18 @@ function Test-TranslationFiles() {
 #region DebugInfo
 Write-AlpacaGroupStart "DebugInfo" #Level 1
 if ($env:RUNNER_DEBUG -eq '1' -or $env:GITHUB_RUN_ATTEMPT -gt 1) {
-    Write-AlpacaOutput "App Type: $appType"
+    Write-AlpacaOutput "App Type: $AppType"
 
     Write-AlpacaGroupStart "Compilation Params:" #Level 2
-    "$($compilationParams.Value | ConvertTo-Json -Depth 2)" -split "`n" | ForEach-Object { Write-AlpacaOutput $_ }
+    "$($CompilationParams.Value | ConvertTo-Json -Depth 2)" -split "`n" | ForEach-Object { Write-AlpacaOutput $_ }
     Write-AlpacaGroupEnd #Level 2
 }
 $Settings = $env:Settings | ConvertFrom-Json
 Write-AlpacaOutput "Settings:"
-Write-AlpacaOutput ("Settings.alpaca.createTranslations = {0}" -f $(try { $Settings.alpaca.createTranslations }catch {}))
-Write-AlpacaOutput ("Settings.alpaca.translationLanguages = {0}" -f $(try { $Settings.alpaca.translationLanguages -join ', ' }catch {}))
-Write-AlpacaOutput ("Settings.alpaca.TestTranslations = {0}" -f $(try { $Settings.alpaca.TestTranslations }catch {}))
-Write-AlpacaOutput ("Settings.alpaca.testTranslationRules = {0}" -f $(try { $Settings.alpaca.testTranslationRules -join ', ' }catch {}))
+Write-AlpacaOutput ("Settings.alpaca.createTranslations = {0}" -f $(try { $Settings.alpaca.createTranslations }catch { "" }))
+Write-AlpacaOutput ("Settings.alpaca.translationLanguages = {0}" -f $(try { $Settings.alpaca.translationLanguages -join ', ' }catch { "" }))
+Write-AlpacaOutput ("Settings.alpaca.TestTranslations = {0}" -f $(try { $Settings.alpaca.TestTranslations }catch { "" }))
+Write-AlpacaOutput ("Settings.alpaca.testTranslationRules = {0}" -f $(try { $Settings.alpaca.testTranslationRules -join ', ' }catch { "" }))
 
 Write-AlpacaGroupEnd #Level 1
 #endregion DebugInfo
@@ -125,10 +124,10 @@ if ($Settings.alpaca.PSObject.Properties.Name -notcontains 'translationLanguages
     return
 }
 
-$AppJson = $appJsonContent | ConvertFrom-Json #appJsonContent comes from parent script
+$AppJson = $AppJsonContent | ConvertFrom-Json #appJsonContent comes from parent script
 $TranslationEnabledInAppJson = $AppJson.PSObject.Properties.Name -contains 'features' -and $AppJson.features -contains 'TranslationFile'
 Write-AlpacaOutput "Translation enabled in app.json: $TranslationEnabledInAppJson"
-$TranslationEnforcedByPipelineSetting = $compilationParams.Value.PSObject.Properties.Name -contains 'features' -and $compilationParams.Value.features -contains 'TranslationFile' #Set by buildmodes=Translated
+$TranslationEnforcedByPipelineSetting = $CompilationParams.Value.PSObject.Properties.Name -contains 'features' -and $CompilationParams.Value.features -contains 'TranslationFile' #Set by buildmodes=Translated
 Write-AlpacaOutput "Translation enforced by pipeline setting: $TranslationEnforcedByPipelineSetting"
 if (-not ($TranslationEnabledInAppJson -or $TranslationEnforcedByPipelineSetting)) {
     Write-AlpacaOutput "Translation feature is not enabled in app.json or enforced by pipeline settings. Skipping precompilation and translation."
@@ -141,12 +140,12 @@ Write-AlpacaGroupEnd #Level 1
 Write-AlpacaGroupStart "Precompile and Translate" #Level 1
 
 #region ClearTranslations
-$TranslationFolder = Join-Path $compilationParams.Value.appProjectFolder "Translations"
+$TranslationFolder = Join-Path $CompilationParams.Value.appProjectFolder "Translations"
 if (-not (Test-Path $TranslationFolder)) {
     Write-AlpacaWarning "Translation folder $TranslationFolder does not exist."
 }
 Write-AlpacaOutput "Clearing existing translation files in $TranslationFolder"
-Get-ChildItem $TranslationFolder -Recurse -File -Filter *.xlf | Foreach-Object {
+Get-ChildItem $TranslationFolder -Recurse -File -Filter *.xlf | ForEach-Object {
     Write-AlpacaOutput "Removing translation file: $($_.FullName)"
     Remove-Item $_.FullName -Force -Confirm:$false
 }
@@ -154,28 +153,29 @@ Get-ChildItem $TranslationFolder -Recurse -File -Filter *.xlf | Foreach-Object {
 
 #region PreCompile
 Write-AlpacaOutput "Minimized parameters to speed up compilation"
-$compilationParamsCopy = $compilationParams.Value.Clone()
-$compilationParamsCopy.OutputTo = { Param($line) }
-$compilationParamsCopy.CopyAppToSymbolsFolder = $false
-$compilationParamsCopy.Remove("generatecrossreferences")
-$compilationParamsCopy.Remove("EnablePerTenantExtensionCop")
-$compilationParamsCopy.Remove("EnableAppSourceCop")
-$compilationParamsCopy.updateDependencies = $false
-$compilationParamsCopy.Remove("EnableCodeCop")
-$compilationParamsCopy.Remove("EnableUICop")
+$CompilationParamsCopy = $CompilationParams.Value.Clone()
+$CompilationParamsCopy.OutputTo = { param($Line) }
+$CompilationParamsCopy.CopyAppToSymbolsFolder = $false
+$CompilationParamsCopy.Remove("generatecrossreferences")
+$CompilationParamsCopy.Remove("EnablePerTenantExtensionCop")
+$CompilationParamsCopy.Remove("EnableAppSourceCop")
+$CompilationParamsCopy.updateDependencies = $false
+$CompilationParamsCopy.Remove("EnableCodeCop")
+$CompilationParamsCopy.Remove("EnableUICop")
 
 if ($useCompilerFolder) {
-    $null = Invoke-Command -ScriptBlock $CompileAppWithBcCompilerFolder -ArgumentList $compilationParamsCopy
+    #useCompilerFolder comes from parent scope
+    $null = Invoke-Command -ScriptBlock $CompileAppWithBcCompilerFolder -ArgumentList $CompilationParamsCopy
 }
 else {
-    $null = Invoke-Command -ScriptBlock $CompileAppInBcContainer -ArgumentList $compilationParamsCopy
+    $null = Invoke-Command -ScriptBlock $CompileAppInBcContainer -ArgumentList $CompilationParamsCopy
 }
 #endregion PreCompile
 
 #region Translate
-New-TranslationFiles -Folder $TranslationFolder -Languages $Settings.alpaca.translationLanguages
+New-TranslationFile -Folder $TranslationFolder -Languages $Settings.alpaca.translationLanguages
 if ($Settings.alpaca.PSObject.Properties.Name -contains 'TestTranslations' -and $Settings.alpaca.TestTranslations) {
-    Test-TranslationFiles -Folder $TranslationFolder -Rules $Settings.alpaca.testTranslationRules
+    Test-TranslationFile -Folder $TranslationFolder -Rules $Settings.alpaca.testTranslationRules
 }
 #endregion Translate
 
