aad46f7f5a00558a16a641b698bf5334358485c6
Christoph Blank <christoph.blank@cosmoconsult.com>
2025-11-12 09:26:42 +0100

    feat(CreateContainers): create containers per buildMode

--- Actions/CreateContainers/CreateContainers.ps1
+++ Actions/CreateContainers/CreateContainers.ps1
@@ -7,6 +7,23 @@ Param(
 
 Import-Module (Join-Path -Path $PSScriptRoot -ChildPath "..\..\Scripts\Modules\Alpaca.psd1" -Resolve) -DisableNameChecking
 
+Write-Host "Download and Import AL-Go Helpers"
+$helperFiles = @{  
+    "AL-Go-Helper.ps1"     = "https://raw.githubusercontent.com/microsoft/AL-Go/ab2f5319ed073c542e03914f8ae6c0fda029ee1e/Actions/AL-Go-Helper.ps1"
+    "settings.schema.json" = "https://raw.githubusercontent.com/microsoft/AL-Go/ab2f5319ed073c542e03914f8ae6c0fda029ee1e/Actions/settings.schema.json"
+}
+foreach ($file in $helperFiles.Keys) {
+    $url = $helperFiles[$file]
+    $dest = Join-Path $PSScriptRoot $file
+    try {
+        Invoke-RestMethod -Uri $url -Headers @{ "Authorization" = "token $Token" } -OutFile $dest -ErrorAction Stop
+    }
+    catch {
+        throw "Failed to download ${file} from ${url}: " + $_.Exception.Message
+    }
+}
+. (Join-Path $PSScriptRoot "AL-Go-Helper.ps1")
+
 try {
     $projects = [string[]]("$ProjectsJson" | ConvertFrom-Json)
     if (! $projects) {
@@ -22,13 +39,23 @@ $containers = @()
 
 try {
     foreach ($project in $projects) {
-        $containers += New-AlpacaContainer -Project $project -Token $Token
+        Write-AlpacaOutput "Getting Project Settings for project: '$project'"
+        $ProjectSettings = ReadSettings -project $project
+        if ($ProjectSettings.buildModes.Count -le 0) {
+            Write-AlpacaOutput "No build modes defined for project '$project', using default build mode."
+            $ProjectSettings.buildModes = @("Default")
+        }
+        foreach ($BuildMode in $ProjectSettings.buildModes) {
+            Write-AlpacaOutput "Creating container for project '$project' with build mode '$BuildMode'"
+            $containers += New-AlpacaContainer -Project $project -Token $Token -BuildMode $BuildMode
+        }
     }
-} catch {
+}
+catch {
     Write-AlpacaError "Failed to create containers:`n$($_.Exception.Message)"
     throw "Failed to create containers"
 } finally {
-    Write-AlpacaOutput "Created $($containers.Count) of $($projects.Count) containers"
+    Write-AlpacaOutput "Created $($containers.Count) containers"
 
     $containersJson = $containers | ConvertTo-Json -Depth 99 -Compress -AsArray
     Add-Content -encoding UTF8 -Path $env:GITHUB_ENV -Value "ALPACA_CONTAINERS_JSON=$($containersJson)"
