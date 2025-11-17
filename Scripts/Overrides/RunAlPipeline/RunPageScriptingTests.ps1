77e946297855ef97b809b8d919eeca97b887f256
Christoph Blank <christoph.blank@cosmoconsult.com>
2025-11-17 17:47:28 +0100

    fix(tests): set startAddress parameter from Environment in RunPageScriptingTests.ps1

--- Scripts/Overrides/RunAlPipeline/RunPageScriptingTests.ps1
+++ Scripts/Overrides/RunAlPipeline/RunPageScriptingTests.ps1
@@ -12,6 +12,7 @@ Write-Host "Custom Code Start"
 sudo npx playwright install-deps 
 Write-Host "Custom Code End"
 
+$parameters.startAddress = $parameters.Environment
 RunPageScriptingTests $parameters #invoke parent
 Write-Host "Custom Function - RunPageScriptingTests.ps1 - End"
 
