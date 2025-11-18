b70676eead796f09b68e8dca8711bfa7ea2aed0e
Christoph Blank <christoph.blank@cosmoconsult.com>
2025-11-18 08:57:28 +0100

    refactor(tests): enhance output logging and debugging information in RunPageScriptingTests.ps1

--- Scripts/Overrides/RunAlPipeline/RunPageScriptingTests.ps1
+++ Scripts/Overrides/RunAlPipeline/RunPageScriptingTests.ps1
@@ -5,12 +5,27 @@ param(
 Write-Host "Custom Function - RunPageScriptingTests.ps1 - Start"
 
 Write-AlpacaGroupStart "Parameters"
-$parameters.GetEnumerator() | ForEach-Object { Write-Host "$($_.Key): $($_.Value)" }
+$parameters.GetEnumerator() | ForEach-Object { Write-AlpacaOutput "$($_.Key): $($_.Value)" }
 Write-AlpacaGroupEnd
 
+
+
 Write-Host "Custom Code Start"
-sudo npx playwright install-deps 
+Write-Host "[exec]sudo npx playwright@1.48.0 install-deps "
+sudo npx playwright@1.48.0 install-deps 
+Write-Host "[exec]pwsh -command { npm i @microsoft/bc-replay@0.1.67 --save }"
+pwsh -command { npm i @microsoft/bc-replay@0.1.67 --save }
+Write-AlpacaGroupStart "Debugging Info"
+Write-AlpacaOutput "Current Directory: $(Get-Location)"
+Write-AlpacaGroupStart "Listing Files:"
+Get-ChildItem -Recurse | ForEach-Object { Write-AlpacaOutput $_.FullName }
+Write-AlpacaGroupEnd
+Write-AlpacaGroupEnd
+
 Write-Host "Custom Code End"
+
+
+
 
 $parameters.startAddress = $parameters.Environment
 RunPageScriptingTests $parameters #invoke parent
