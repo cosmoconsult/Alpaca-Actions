3b9381a2ecb3d20c4a4c6067e98e2a4e1a244269
Christoph Blank <christoph.blank@cosmoconsult.com>
2025-11-18 09:41:54 +0100

    refactor(tests): comment out unused code in RunPageScriptingTests.ps1 for clarity

--- Scripts/Overrides/RunAlPipeline/RunPageScriptingTests.ps1
+++ Scripts/Overrides/RunAlPipeline/RunPageScriptingTests.ps1
@@ -8,19 +8,18 @@ Write-AlpacaGroupStart "Parameters"
 $parameters.GetEnumerator() | ForEach-Object { Write-AlpacaOutput "$($_.Key): $($_.Value)" }
 Write-AlpacaGroupEnd
 
-
-
 Write-Host "Custom Code Start"
-Write-Host "[exec]sudo npx playwright@1.48.0 install-deps "
-sudo npx playwright@1.48.0 install-deps 
-Write-Host "[exec]pwsh -command { npm i @microsoft/bc-replay@0.1.67 --save }"
-pwsh -command { npm i @microsoft/bc-replay@0.1.67 --save }
-Write-AlpacaGroupStart "Debugging Info"
-Write-AlpacaOutput "Current Directory: $(Get-Location)"
-Write-AlpacaGroupStart "Listing Files:"
-Get-ChildItem -Recurse | ForEach-Object { Write-AlpacaOutput $_.FullName }
-Write-AlpacaGroupEnd
-Write-AlpacaGroupEnd
+
+# Write-Host "[exec]sudo npx playwright@1.48.0 install-deps "
+# sudo npx playwright@1.48.0 install-deps 
+# Write-Host "[exec]pwsh -command { npm i @microsoft/bc-replay@0.1.67 --save }"
+# pwsh -command { npm i @microsoft/bc-replay@0.1.67 --save }
+# Write-AlpacaGroupStart "Debugging Info"
+# Write-AlpacaOutput "Current Directory: $(Get-Location)"
+# Write-AlpacaGroupStart "Listing Files:"
+# Get-ChildItem -Recurse | ForEach-Object { Write-AlpacaOutput $_.FullName }
+# Write-AlpacaGroupEnd
+# Write-AlpacaGroupEnd
 
 Write-Host "Custom Code End"
 
