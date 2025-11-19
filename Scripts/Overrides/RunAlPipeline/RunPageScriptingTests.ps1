cb7195b31bf4c76590257eb8910c40481718c5d3
Christoph Blank <christoph.blank@cosmoconsult.com>
2025-11-19 11:11:53 +0100

    fix(tests): update credential handling in RunPageScriptingTests.ps1 for improved security

--- Scripts/Overrides/RunAlPipeline/RunPageScriptingTests.ps1
+++ Scripts/Overrides/RunAlPipeline/RunPageScriptingTests.ps1
@@ -28,8 +28,8 @@ Write-AlpacaOutput "Custom Code End"
 Write-AlpacaOutput "Overriding start address to Environment value: $($parameters.Environment)"
 $parameters.startAddress = $parameters.Environment
 
-Write-AlpacaOutput "Overriding credential to BcAuthContext credential (User: $($bcAuthContext.Credential.UserName))"
-$parameters.credential = $bcAuthContext.Credential
+Write-AlpacaOutput ("Overriding credential to BcAuthContext credential (User: {0})" -f $(try { $bcAuthContext.username }catch { "" }))
+$parameters.credential = New-Object System.Management.Automation.PSCredential ($bcAuthContext.username, (ConvertTo-SecureString $bcAuthContext.Password -AsPlainText -Force))
 
 Write-AlpacaOutput "Invoking parent RunPageScriptingTests with modified parameters"
 RunPageScriptingTests $parameters #invoke parent
