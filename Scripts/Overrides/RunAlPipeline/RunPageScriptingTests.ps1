abdfdb8a2e73f981f5bc315aa123e2762f934226
Christoph Blank <christoph.blank@cosmoconsult.com>
2025-11-19 13:09:11 +0100

    fix(tests): update credential handling in RunPageScriptingTests.ps1 to use plain password for improved compatibility

--- Scripts/Overrides/RunAlPipeline/RunPageScriptingTests.ps1
+++ Scripts/Overrides/RunAlPipeline/RunPageScriptingTests.ps1
@@ -29,7 +29,7 @@ Write-AlpacaOutput "Overriding start address to Environment value: $($parameters
 $parameters.startAddress = $parameters.Environment
 
 Write-AlpacaOutput ("Overriding credential to BcAuthContext credential (User: {0})" -f $(try { $bcAuthContext.username }catch { "" }))
-$parameters.credential = New-Object System.Management.Automation.PSCredential ($bcAuthContext.username, (ConvertTo-SecureString $bcAuthContext.Password -AsPlainText -Force))
+$parameters.credential = New-Object System.Management.Automation.PSCredential ($bcAuthContext.username, $bcAuthContext.Password)
 
 Write-AlpacaOutput "Invoking parent RunPageScriptingTests with modified parameters"
 RunPageScriptingTests $parameters #invoke parent
