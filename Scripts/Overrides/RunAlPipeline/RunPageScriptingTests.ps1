018e00d2320d211694292ed8cd5c1c51397ee7cd
Christoph Blank <christoph.blank@cosmoconsult.com>
2025-11-28 14:06:06 +0100

    refactor(RunPageScriptingTests): update parameter handling for improved clarity and functionality

--- Scripts/Overrides/RunAlPipeline/RunPageScriptingTests.ps1
+++ Scripts/Overrides/RunAlPipeline/RunPageScriptingTests.ps1
@@ -1,23 +1,35 @@
 param(
-    [Hashtable] $parameters
+    [string] $containerName,
+    [PSCredential] $credential,
+    [array] $pageScriptingTests,
+    [array] $restoreDatabases,
+    [string] $pageScriptingTestResultsFile,
+    [string] $pageScriptingTestResultsFolder,
+    [string] $startAddress,
+    [scriptblock] $RestoreDatabasesInBcContainer,
+    [switch] $returnTrueIfAllPassed
 )
 Write-AlpacaOutput "Using COSMO Alpaca override"
 
 if ($env:RUNNER_DEBUG -eq "1") {
     Write-AlpacaGroupStart "Parameters"
     try {
-        $parameters.GetEnumerator() | ForEach-Object { Write-AlpacaDebug "$($_.Key): $($_.Value)" }
+        $PSBoundParameters.GetEnumerator() | ForEach-Object { Write-AlpacaDebug "$($_.Key): $($_.Value)" }
     }
     finally {
         Write-AlpacaGroupEnd
     }
 }
 
-Write-AlpacaOutput "Overriding start address to Environment value: $($parameters.Environment)"
-$parameters.startAddress = $parameters.Environment
+Write-AlpacaOutput "Overriding start address to Environment value: $($environment)" # $environment comes from parent script
+$startAddress = $environment
 
-Write-AlpacaOutput ("Overriding credential to BcAuthContext credential (User: {0})" -f $(try { $bcAuthContext.username }catch { "" }))
-$parameters.credential = New-Object System.Management.Automation.PSCredential ($bcAuthContext.username, $bcAuthContext.Password)
+Write-AlpacaOutput ("Overriding credential to BcAuthContext credential (User: {0})" -f $(try { $bcAuthContext.username }catch { "" })) # bcAuthContext comes from parent script
+$credential = New-Object System.Management.Automation.PSCredential ($bcAuthContext.username, $bcAuthContext.Password)
 
+Write-AlpacaOutput "Rebuild Param Object with modified parameters"
+$UpdatedParams = @{}
+$UpdatedParams += $PSBoundParameters.GetEnumerator() | ForEach-Object { @{"$($PSBoundParameters[$_.Key])" = $(Get-Variable -Name $_.Key -ValueOnly) } }
+        
 Write-AlpacaOutput "Invoking parent RunPageScriptingTests with modified parameters"
-RunPageScriptingTests $parameters #invoke parent
+RunPageScriptingTests @UpdatedParams # invoke parent
