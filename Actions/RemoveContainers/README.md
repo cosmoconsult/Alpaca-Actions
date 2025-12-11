19ba6db854e8cbead5d1dd3aa63a5d54c3a760cc
Ole Wunschmann <ole.wunschmann@cosmoconsult.com>
2025-12-12 00:04:59 +0100

    feat(containers): Optional filters for action "RemoveContainers"

--- Actions/RemoveContainers/README.md
+++ Actions/RemoveContainers/README.md
@@ -17,8 +17,7 @@ Remove COSMO Alpaca containers
 | shell | | The shell (powershell or pwsh) in which the PowerShell script in this action should run | powershell |
 | token | | The GitHub token running the action | github.token |
 | containersJson | Yes | An array of Alpaca container informations in compressed JSON format | |
-| project | | Optional project name to filter containers to remove | |
-| buildMode | | Optional build mode to filter containers to remove | |
+| filterJson | | Optional Alpaca container information to filter containers by in compressed JSON format | |
 
 ## OUTPUT
 
--- Actions/RemoveContainers/RemoveContainers.ps1
+++ Actions/RemoveContainers/RemoveContainers.ps1
@@ -3,10 +3,8 @@ param (
     [string] $Token,
     [Parameter(HelpMessage = "An array of Alpaca container informations in compressed JSON format", Mandatory = $true)]
     [string] $ContainersJson,
-    [Parameter(HelpMessage = "The AL-Go project identifier", Mandatory = $false)]
-    [string] $Project = $null,
-    [Parameter(HelpMessage = "The AL-Go build mode", Mandatory = $false)]
-    [string] $BuildMode = $null
+    [Parameter(HelpMessage = "Optional Alpaca container information to filter containers by in compressed JSON format", Mandatory = $false)]
+    [string] $FilterJson
 )
 
 Import-Module (Join-Path -Path $PSScriptRoot -ChildPath "..\..\Scripts\Modules\Alpaca.psd1" -Resolve) -DisableNameChecking
@@ -15,13 +13,14 @@ try {
     Write-AlpacaGroupStart "Determine containers"
 
     $containers = [pscustomobject[]]("$ContainersJson" | ConvertFrom-Json)
-    if ($null -ne $Project) {
-        Write-AlpacaOutput "Filtering by project '$Project'"
-        $containers = [pscustomobject[]]($containers | Where-Object { $_.Project -eq $Project })
-    }
-    if ($null -ne $BuildMode) {
-        Write-AlpacaOutput "Filtering by build mode '$BuildMode'"
-        $containers = [pscustomobject[]]($containers | Where-Object { $_.BuildMode -eq $BuildMode })
+
+    $filter = "$FilterJson" | ConvertFrom-Json
+    if ($filter) {
+        foreach ($key in $filter.PSObject.Properties.Name) {
+            $value = $filter.$key
+            Write-AlpacaOutput "Filtering by '$key' = '$value'"
+            $containers = [pscustomobject[]]($containers | Where-Object { $_.$key -eq $value })
+        }
     }
     Write-AlpacaOutput "Determined $($containers.Count) containers:"
     foreach ($container in $containers) {
