196a1118cadae533cc2e66376e06deb2ea5331b4
Ole Wunschmann <ole.wunschmann@cosmoconsult.com>
2025-12-11 23:13:44 +0100

    feat(containers): Optional filters for action "RemoveContainers"

--- Actions/RemoveContainers/README.md
+++ Actions/RemoveContainers/README.md
@@ -17,6 +17,8 @@ Remove COSMO Alpaca containers
 | shell | | The shell (powershell or pwsh) in which the PowerShell script in this action should run | powershell |
 | token | | The GitHub token running the action | github.token |
 | containersJson | Yes | An array of Alpaca container informations in compressed JSON format | |
+| project | | Optional project name to filter containers to remove | |
+| buildMode | | Optional build mode to filter containers to remove | |
 
 ## OUTPUT
 
--- Actions/RemoveContainers/RemoveContainers.ps1
+++ Actions/RemoveContainers/RemoveContainers.ps1
@@ -2,26 +2,46 @@ param (
     [Parameter(HelpMessage = "The GitHub token running the action", Mandatory = $true)]
     [string] $Token,
     [Parameter(HelpMessage = "An array of Alpaca container informations in compressed JSON format", Mandatory = $true)]
-    [string] $ContainersJson
+    [string] $ContainersJson,
+    [Parameter(HelpMessage = "The AL-Go project identifier", Mandatory = $false)]
+    [string] $Project = $null,
+    [Parameter(HelpMessage = "The AL-Go build mode", Mandatory = $false)]
+    [string] $BuildMode = $null
 )
 
 Import-Module (Join-Path -Path $PSScriptRoot -ChildPath "..\..\Scripts\Modules\Alpaca.psd1" -Resolve) -DisableNameChecking
 
 try {
+    Write-AlpacaGroupStart "Determine containers"
+
     $containers = [pscustomobject[]]("$ContainersJson" | ConvertFrom-Json)
-    Write-AlpacaOutput "Deleting containers: '$($containers.Id -join "', '")' [$($containers.Count)]"
+    if ($null -ne $Project) {
+        Write-AlpacaOutput "Filtering by project '$Project'"
+        $containers = [pscustomobject[]]($containers | Where-Object { $_.Project -eq $Project })
+    }
+    if ($null -ne $BuildMode) {
+        Write-AlpacaOutput "Filtering by build mode '$BuildMode'"
+        $containers = [pscustomobject[]]($containers | Where-Object { $_.BuildMode -eq $BuildMode })
+    }
+    Write-AlpacaOutput "Determined $($containers.Count) containers:"
+    foreach ($container in $containers) {
+        Write-AlpacaOutput "- Id: '$($container.Id)', Project: '$($container.Project)', BuildMode: '$($container.BuildMode)'"
+    }
+
+    Write-AlpacaGroupEnd
 } 
 catch {
     throw "Failed to determine containers: $($_.Exception.Message)"
 }
 
+Write-AlpacaGroupStart "Deleting containers"
 $failures = 0
 
 foreach ($container in $containers) {
     try {
         Remove-AlpacaContainer -Container $container -Token $Token
     } catch {
-        Write-AlpacaError "Failed to delete container '$($container.Id)':`n$($_.Exception.Message)"
+        Write-AlpacaError "Failed to delete container '$($container.Id)':`n$($_.Exception.Message)" -WithoutGitHubAnnotation
         $failures += 1
     }
 }
@@ -30,3 +50,5 @@ Write-AlpacaOutput "Deleted $($containers.Count - $failures) of $($containers.Co
 if ($failures) {
     throw "Failed to delete $failures containers"
 }
+
+Write-AlpacaGroupEnd
