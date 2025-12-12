51f3a91c089fecfc123c6a3114658f40117bb068
Ole Wunschmann <44057549+OleWunschmann@users.noreply.github.com>
2025-12-12 15:10:12 +0100

    feat(containers): Update action "RemoveContainer" with optional filter and 404 error suppression (#21)
    [AB#4588](https://dev.azure.com/cc-ppi/83f75d99-795d-45dc-8543-9fe1918ff7f9/_workitems/edit/4588)

--- Actions/RemoveContainers/RemoveContainers.ps1
+++ Actions/RemoveContainers/RemoveContainers.ps1
@@ -2,19 +2,39 @@ param (
     [Parameter(HelpMessage = "The GitHub token running the action", Mandatory = $true)]
     [string] $Token,
     [Parameter(HelpMessage = "An array of Alpaca container informations in compressed JSON format", Mandatory = $true)]
-    [string] $ContainersJson
+    [string] $ContainersJson,
+    [Parameter(HelpMessage = "Optional Alpaca container information to filter containers by in JSON format", Mandatory = $false)]
+    [string] $FilterJson
 )
 
 Import-Module (Join-Path -Path $PSScriptRoot -ChildPath "..\..\Scripts\Modules\Alpaca.psd1" -Resolve) -DisableNameChecking
 
 try {
+    Write-AlpacaGroupStart "Determine containers"
+
     $containers = [pscustomobject[]]("$ContainersJson" | ConvertFrom-Json)
-    Write-AlpacaOutput "Deleting containers: '$($containers.Id -join "', '")' [$($containers.Count)]"
+
+    $filter = "$FilterJson" | ConvertFrom-Json
+    if ($filter) {
+        foreach ($key in $filter.PSObject.Properties.Name) {
+            $value = $filter.$key
+            Write-AlpacaOutput "Filtering by '$key' = '$value'"
+            $containers = [pscustomobject[]]($containers | Where-Object { $_.$key -eq $value })
+        }
+    }
+    Write-AlpacaOutput "Determined $($containers.Count) containers:"
+    foreach ($container in $containers) {
+        Write-AlpacaOutput "- Id: '$($container.Id)', Project: '$($container.Project)', BuildMode: '$($container.BuildMode)'"
+    }
 } 
 catch {
     throw "Failed to determine containers: $($_.Exception.Message)"
 }
+finally {
+    Write-AlpacaGroupEnd
+}
 
+Write-AlpacaGroupStart "Deleting $($containers.Count) containers"
 $failures = 0
 
 foreach ($container in $containers) {
@@ -27,6 +47,8 @@ foreach ($container in $containers) {
 }
 
 Write-AlpacaOutput "Deleted $($containers.Count - $failures) of $($containers.Count) containers"
+Write-AlpacaGroupEnd
+
 if ($failures) {
     throw "Failed to delete $failures containers"
 }
--- Scripts/Modules/Remove-AlpacaContainer.psm1
+++ Scripts/Modules/Remove-AlpacaContainer.psm1
@@ -11,14 +11,24 @@ function Remove-AlpacaContainer {
     $repository = $repository.replace($owner, "")
     $repository = $repository.replace("/", "")
 
-    Write-AlpacaOutput "Deleting Container '$($Container.Id)' of project '$($Container.Project)'"
+    try {
+        Write-AlpacaGroupStart "Deleting Container '$($Container.Id)' (Project: '$($Container.Project)', BuildMode: '$($Container.BuildMode)')"
+        
+        $headers = Get-AlpacaAuthenticationHeaders -Token $Token -Owner $owner -Repository $repository
 
-    $headers = Get-AlpacaAuthenticationHeaders -Token $Token -Owner $owner -Repository $repository
+        $apiUrl = Get-AlpacaEndpointUrlWithParam -Api 'alpaca' -Controller "Container" -Endpoint "Container" -Ressource $Container.Id
+        
+        Invoke-RestMethod $apiUrl -Method 'DELETE' -Headers $headers -AllowInsecureRedirect | Out-Null
 
-    $apiUrl = Get-AlpacaEndpointUrlWithParam -Api 'alpaca' -Controller "Container" -Endpoint "Container" -Ressource $Container.Id
-    
-    Invoke-RestMethod $apiUrl -Method 'DELETE' -Headers $headers -AllowInsecureRedirect | Out-Null
-
-    Write-AlpacaOutput "Deleted Container '$($Container.Id)'"
+        Write-AlpacaOutput "Container '$($Container.Id)' deleted"
+    } catch {
+        if ($_.Exception.StatusCode -eq [System.Net.HttpStatusCode]::NotFound) {
+            Write-AlpacaOutput "Container '$($Container.Id)' not found"
+        } else {
+            throw
+        }
+    } finally {
+        Write-AlpacaGroupEnd
+    }
 }
 Export-ModuleMember -Function Remove-AlpacaContainer
