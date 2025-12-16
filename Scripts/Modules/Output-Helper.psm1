304c800af38a3ba488d9cdc2f754decbff925f8d
Ole Wunschmann <ole.wunschmann@cosmoconsult.com>
2025-12-16 21:16:27 +0100

    Disable xmas fun

--- Scripts/Modules/Output-Helper.psm1
+++ Scripts/Modules/Output-Helper.psm1
@@ -120,14 +120,16 @@ function Write-AlpacaOutput {
     $linePrefix = $script:groupIndentation * $script:groupLevel;
     $lineSuffix = ""
 
-    $date = Get-Date
-    if ($date.Month -eq 12 -and $date.Day -le 26) {
-        $emoji = $null
-        while ($emoji -in $null, $script:xmasEmojiLastUsed) {
-            $emoji = $script:xmasEmojis | Get-Random
+    if ($false) {
+        $date = Get-Date
+        if ($date.Month -eq 12 -and $date.Day -in 24,25,26) {
+            $emoji = $null
+            while ($emoji -in $null, $script:xmasEmojiLastUsed) {
+                $emoji = $script:xmasEmojis | Get-Random
+            }
+            $script:xmasEmojiLastUsed = $emoji
+            $lineSuffix = " $emoji"
         }
-        $script:xmasEmojiLastUsed = $emoji
-        $lineSuffix = " $emoji"
     }
 
     $formattedMessage = Format-AlpacaMessage -Message $Message -Color $Color -LinePrefix $linePrefix -LineSuffix $lineSuffix
