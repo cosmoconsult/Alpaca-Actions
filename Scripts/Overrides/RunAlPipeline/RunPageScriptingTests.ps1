2b4d18541f57b5b20e94d6e48e13ab5509efd9d4
Christoph Blank <christoph.blank@cosmoconsult.com>
2026-03-18 16:33:53 +0100

    fix(parameters): update variable assignments for start address and credential to use local scope

--- Scripts/Overrides/RunAlPipeline/RunPageScriptingTests.ps1
+++ Scripts/Overrides/RunAlPipeline/RunPageScriptingTests.ps1
@@ -30,10 +30,13 @@ if ($env:RUNNER_DEBUG -eq "1") {
 }
 
 Write-AlpacaOutput "Overriding start address to Environment value: $($environment)" # $environment comes from parent script
-$startAddress = $environment
+$local_startAddress = $environment
+Set-Variable -Name 'startAddress' -Value $local_startAddress -Scope 1
+
 
 Write-AlpacaOutput ("Overriding credential to BcAuthContext credential (User: {0})" -f $(try { $bcAuthContext.username }catch { "" })) # bcAuthContext comes from parent script
-$credential = New-Object System.Management.Automation.PSCredential ($bcAuthContext.username, $bcAuthContext.Password)
+$local_credential = New-Object System.Management.Automation.PSCredential ($bcAuthContext.username, $bcAuthContext.Password)
+Set-Variable -Name 'credential' -Value $local_credential -Scope 1
 
 Write-AlpacaOutput "Rebuild Param Object with modified parameters"
 $UpdatedParams = @{}
