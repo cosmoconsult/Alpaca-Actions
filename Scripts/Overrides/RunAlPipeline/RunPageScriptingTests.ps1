016537170c3ca39838ed964125f0fb24af27d6b1
Christoph Blank <christoph.blank@cosmoconsult.com>
2026-03-19 09:32:51 +0100

    refactor(parameters): consolidate parameters into a hashtable for improved readability and maintainability

--- Scripts/Overrides/RunAlPipeline/RunPageScriptingTests.ps1
+++ Scripts/Overrides/RunAlPipeline/RunPageScriptingTests.ps1
@@ -1,20 +1,12 @@
 param(
-    [string] $containerName,
-    [PSCredential] $credential,
-    [array] $pageScriptingTests,
-    [array] $restoreDatabases,
-    [string] $pageScriptingTestResultsFile,
-    [string] $pageScriptingTestResultsFolder,
-    [string] $startAddress,
-    [scriptblock] $RestoreDatabasesInBcContainer,
-    [switch] $returnTrueIfAllPassed
+    [hashtable] $params
 )
 Write-AlpacaOutput "Using COSMO Alpaca override"
 
 if ($env:RUNNER_DEBUG -eq "1") {
     Write-AlpacaGroupStart "Parameters"
     try {
-        $PSBoundParameters.GetEnumerator() | ForEach-Object { 
+        $params.GetEnumerator() | ForEach-Object { 
             $value = if ($_.Value -is [array] -or $_.Value -is [hashtable]) {
                 $_.Value | ConvertTo-Json -Compress
             }
@@ -30,17 +22,10 @@ if ($env:RUNNER_DEBUG -eq "1") {
 }
 
 Write-AlpacaOutput "Overriding start address to Environment value: $($environment)" # $environment comes from parent script
-$local_startAddress = $environment
-Set-Variable -Name 'startAddress' -Value $local_startAddress -Scope 1
-
+Set-Variable -Name 'startAddress' -Value $environment -Scope 1 # directly modify the variable in the parent scope
 
 Write-AlpacaOutput ("Overriding credential to BcAuthContext credential (User: {0})" -f $(try { $bcAuthContext.username }catch { "" })) # bcAuthContext comes from parent script
-$local_credential = New-Object System.Management.Automation.PSCredential ($bcAuthContext.username, $bcAuthContext.Password)
-Set-Variable -Name 'credential' -Value $local_credential -Scope 1
-
-Write-AlpacaOutput "Rebuild Param Object with modified parameters"
-$UpdatedParams = @{}
-$UpdatedParams += $PSBoundParameters.GetEnumerator() | ForEach-Object { @{"$($PSBoundParameters[$_.Key])" = $(Get-Variable -Name $_.Key -ValueOnly) } }
+$params.credential = New-Object System.Management.Automation.PSCredential ($bcAuthContext.username, $bcAuthContext.Password) # modify the hashtable parameter
         
-Write-AlpacaOutput "Invoking parent RunPageScriptingTests with modified parameters"
-RunPageScriptingTests @UpdatedParams # invoke parent
+Write-AlpacaOutput "Invoking parent RunPageScriptingTests with modified parameters: $($params | ConvertTo-Json -Compress)"
+RunPageScriptingTests @params # invoke parent
